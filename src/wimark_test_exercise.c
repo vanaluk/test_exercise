1b5247d95563e91ee1e2fe8c27d2f5ba011e51fa
ivan.lukianenko <ivan.lukianenko@dsr-corporation.com>
2023-01-13 15:00:09 +0300

    Add start file

---
+++ src/wimark_test_exercise.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : wimark_test_exercise.c
+ Author      : vanaluk
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("wimark_test_exercise"); /* prints wimark_test_exercise */
+	return EXIT_SUCCESS;
+}
